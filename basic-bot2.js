8393c1264f550070ba046d5c97c7d4340fc229c5
Sarp <30959934+SarpTurker@users.noreply.github.com>
2017-12-19 20:58:51 +0300

    Create basic-bot2.js

---
+++ basic-bot2.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./settings/config.json");
+
+// Set the prefix
+let prefix = config.prefix;
+client.on("message", (message) => {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
+
+client.login(config.token);
